5671f6dd3ce1f5585c1513ddea653baa4c70a25d
Tomoatsu Shimada <shimarin@gmail.com>
2025-05-26 19:00:13 +0900

    make apple music installable

--- webappviewer_apps/applemusic.py
+++ webappviewer_apps/applemusic.py
@@ -2,10 +2,11 @@ from PyQt6.QtCore import QUrl
 
 desktop = {
     "name": "Apple Music",
+    "url": "https://music.apple.com/jp/",
     "categories": "Audio;Player;"
 }
 
 def open(window, browser):
-    browser.load(QUrl("https://music.apple.com/jp/"))
+    browser.load(QUrl(desktop["url"]))
     window.resize(1000, 600)
-    window.setWindowTitle("Apple Music")
+    window.setWindowTitle(desktop["name"])

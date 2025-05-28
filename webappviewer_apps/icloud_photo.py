ce60eb36af66adf5c14563175ba6595eb1e119f7
Tomoatsu Shimada <shimarin@gmail.com>
2025-05-28 12:01:12 +0900

    icon added

--- webappviewer_apps/icloud_photo.py
+++ webappviewer_apps/icloud_photo.py
@@ -3,6 +3,7 @@ from PyQt6.QtCore import QUrl
 desktop = {
     "name": "iCloud Photos",
     "url": "https://www.icloud.com/photos/",
+    "icon": "https://www.icloud.com/system/icloud.com/2518Project43/7e9d65d32973291b9c02698411402cbc.png",
     "categories": "Graphics;Photography;"
 }
 
